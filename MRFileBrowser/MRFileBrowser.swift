51c77deb263c158226b8c5420e9fc6b8839bf49c
mukilarasan-ravi <mukilarasan.ravi@gmail.com>
2025-11-30 18:30:41 +0530

    Initial Commit

---
+++ MRFileBrowser/MRFileBrowser.swift
@@ -0,0 +1,9 @@
+//
+//  MRFileBrowser.swift
+//  MRFileBrowser
+//
+//  Created by Mukilarasan Ravi on 30/11/25.
+//
+
+import Foundation
+
---
+++ MRFileBrowserTests/MRFileBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  MRFileBrowserTests.swift
+//  MRFileBrowserTests
+//
+//  Created by Mukilarasan Ravi on 30/11/25.
+//
+
+import Testing
+@testable import MRFileBrowser
+
+struct MRFileBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
